791014601f30dd0b7594d27f7e5cf51dd6713e0f
Smita Singh <85050285+smita198@users.noreply.github.com>
2023-05-28 12:14:11 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PathfindingVisualizer from "./PathfindingVisualizer/PathfindingVisualizer";
+import "./App.css";
+function App() {
+  return (
+    <div className="App">
+      <PathfindingVisualizer/>
+    </div>
+  );
+}
+
+export default App;
